106beddd4bc0091485cd81ac20cd5335ba9918fe
newton_miku <cto@ddxnb.tk>
2022-04-19 18:42:18 +0800

    .

--- cheat/gui/overlay.cpp
+++ cheat/gui/overlay.cpp
@@ -5,6 +5,9 @@ extern int aim;
 extern bool aim_enable;
 extern bool esp;
 
+extern int aSmoothAmount; // Aimbot smoothness
+extern int xFOV; //Aimbot horizontal FOV (square)
+extern int yFOV; //Aimbot vertical FOV (square)
 
 extern bool player_glow;
 extern float playerglow1[4];
@@ -30,9 +33,11 @@ extern float blue_item_col[4];
 
 extern int8_t item_main_glow_type;
 extern int8_t item_border_glow_type;
+extern float item_glow_distance;
 
 extern int8_t player_main_glow_type;
 extern int8_t player_border_glow_type;
+extern float player_glow_distance;
 
 extern bool zoom_glow;
 extern int zoom_num;
@@ -121,12 +126,12 @@ void CleanupRenderTarget();
 
 void Overlay::RenderMenu()
 {
-	
+
 	static bool vis_check = false;
 	static bool spec_disable = false;
 	static bool all_spec_disable = false;
 
-	if (aim > 0)
+	/*if (aim > 0)
 	{
 		aim_enable = true;
 		if (aim > 1)
@@ -142,7 +147,7 @@ void Overlay::RenderMenu()
 	{
 		aim_enable = false;
 		vis_check = false;
-	}
+	}*/
 	static ImGuiColorEditFlags alpha_flags = 0;
 	ImGui::SetNextWindowPos(ImVec2(0, 0));
 	ImGui::SetNextWindowSize(ImVec2(700, 500));
@@ -151,11 +156,10 @@ void Overlay::RenderMenu()
 	{
 		if (ImGui::BeginTabItem(XorStr(u8"物品发光")))
 		{
-			/*ImGui::Checkbox(XorStr("ESP"), &esp);
+			ImGui::Checkbox(XorStr("ESP"), &esp);
 
-			ImGui::Checkbox(XorStr("AIM"), &aim_enable);
 
-			if (aim_enable)
+			/*if (aim_enable)
 			{
 				ImGui::SameLine();
 				ImGui::Checkbox(XorStr("Visibility check"), &vis_check);
@@ -174,21 +178,21 @@ void Overlay::RenderMenu()
 			{
 				aim = 0;
 			}*/
-			
+
 			static int glow_item_index = -1;
 			ImGui::Checkbox(XorStr(u8"物品发光"), &item_glow);
 			static bool output_only_modified = true;
-			
+
 			if (item_glow)
 			{
-				ImGui::Checkbox(XorStr(u8"空投枪"), &glow_supply_gun);
+				ImGui::Checkbox(XorStr(u8"空投枪和红甲"), &glow_supply_gun);
 				ImGui::Checkbox(XorStr(u8"金枪"), &glow_goldgun);
 				ImGui::Checkbox(XorStr(u8"涡轮"), &turbo_glow);
 				ImGui::Checkbox(XorStr(u8"加速装填器"), &fast_reload_glow);
 				ImGui::Checkbox(XorStr(u8"紫金装备"), &glow_suit);
 				ImGui::Checkbox(XorStr(u8"紫金配件"), &glow_gunPart);
 				ImGui::Checkbox(XorStr(u8"枪械发光"), &glow_gun);
-				
+
 				if (glow_gun) {
 					if (ImGui::Combo(u8"总发光类型", &glow_item_index, u8"敖犬\0 Lstar\0哈沃克\0专注\0三重\0平行\0汗落\0转换者\0 r99\0猎兽\0长弓\0滋崩\0 r301\0 eva8\0莫桑比克\0和平喷\0小帮手\0 p2020\0 re45\0哨兵\0弓\0 3030\0暴走\0 car\0\0"))
 					{
@@ -227,7 +231,7 @@ void Overlay::RenderMenu()
 			ImGui::EndTabItem();
 		}
 		if (item_glow) {
-			if (ImGui::BeginTabItem(XorStr(u8"蓝色物资发光"))) 
+			if (ImGui::BeginTabItem(XorStr(u8"蓝色物资发光")))
 			{
 				ImGui::Checkbox(XorStr(u8"蓝色物资"), &blue_glow);
 				if (blue_glow) {
@@ -239,72 +243,76 @@ void Overlay::RenderMenu()
 				ImGui::EndTabItem();
 			}
 		}
-		
+
 		if (item_glow) {
-			if (ImGui::BeginTabItem(XorStr(u8"物品发光设置"))) 
+			if (ImGui::BeginTabItem(XorStr(u8"物品发光设置")))
 			{
-				ImGui::Text(XorStr(u8"最大检测数目")); 
+				ImGui::Text(XorStr(u8"最大检测数目"));
 				ImGui::SameLine();
-				//HelpMarker(u8"将该值调低可能会影响物品发光检测范围，甚至可能检测不到\n但较低的值可降低检测敌人延迟");
-				ImGui::SliderInt(XorStr("##3"), &max_check_glow_item_num, 5000, 50000, "%d");
+				HelpMarker(u8"将该值调低可能会影响物品发光检测范围，甚至可能检测不到\n但较低的值可降低检测敌人延迟");
+				ImGui::SliderInt(XorStr("##3"), &max_check_glow_item_num, 1000, 100000, "%d");
 				static int main_glow1 = -1;
 				static int border_glow1 = -1;
 				ImGui::ColorEdit4(u8"蓝色品质", (float*)blue_item_col, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 				ImGui::ColorEdit4(u8"紫色品质", (float*)purple_item_col, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 				ImGui::ColorEdit4(u8"金色品质", (float*)gold_item_col, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 				ImGui::ColorEdit4(u8"枪械发光颜色", (float*)gun_glow_col, ImGuiColorEditFlags_AlphaBar | alpha_flags);
+				ImGui::Text(XorStr(u8"发光距离")); ImGui::SameLine();
+				HelpMarker(u8"可调整范围为0-800m,ctrl+鼠标单击即可输入值");
+				//ImGui::SliderFloat(XorStr("#dis"), &item_glow_distance, 0.0f, 10000.0f, "%.0fm");//此为滑块调整条，疑似无法手动输入值
+				ImGui::DragFloat(XorStr("#dis"), &item_glow_distance, 50.0f, 0.0f, 800.0f, "%.0fm");
 				ImGui::Text(u8"物品发光类型设置");
 				if (ImGui::Combo(u8"总发光类型", &main_glow1, u8"1\0 2\0 3\0 4\0 5\0 6\0\0"))
 				{
 					switch (main_glow1)
 					{
-						case 0: item_main_glow_type = 101; break;
-						case 1: item_main_glow_type = 102; break;
-						case 2: item_main_glow_type = 103; break;
-						case 3: item_main_glow_type = 104; break;
-						case 4: item_main_glow_type = 105; break;
-						case 5: item_main_glow_type = 106; break;
+					case 0: item_main_glow_type = 101; break;
+					case 1: item_main_glow_type = 102; break;
+					case 2: item_main_glow_type = 103; break;
+					case 3: item_main_glow_type = 104; break;
+					case 4: item_main_glow_type = 105; break;
+					case 5: item_main_glow_type = 106; break;
 					}
 				}
 				if (ImGui::Combo(u8"边界发光类型", &border_glow1, u8"1\0 2\0 3\0 4\0 5\0 6\0\0"))
 				{
 					switch (border_glow1)
 					{
-						case 0: item_border_glow_type = 101; break;
-						case 1: item_border_glow_type = 102; break;
-						case 2: item_border_glow_type = 103; break;
-						case 3: item_border_glow_type = 104; break;
-						case 4: item_border_glow_type = 105; break;
-						case 5: item_border_glow_type = 106; break;
+					case 0: item_border_glow_type = 101; break;
+					case 1: item_border_glow_type = 102; break;
+					case 2: item_border_glow_type = 103; break;
+					case 3: item_border_glow_type = 104; break;
+					case 4: item_border_glow_type = 105; break;
+					case 5: item_border_glow_type = 106; break;
 					}
 				}
 				ImGui::EndTabItem();
 			}
 		}
-		if(item_glow){
+		if (item_glow) {
 			if (ImGui::BeginTabItem(XorStr(u8"倍镜发光")))
 			{
-				ImGui::Checkbox(XorStr(u8"倍镜发光"), &zoom_glow);	
-				if (zoom_glow){
+				ImGui::Checkbox(XorStr(u8"倍镜发光"), &zoom_glow);
+				if (zoom_glow) {
 					ImGui::Combo(u8"总发光类型", &zoom_num, u8"1x\0 2x\0圆1x\0 1-2x\0金1x\0 3x\0 2-4x\0 6x\0 4-8x\0 4-10x\0\0");
 					ImGui::ColorEdit4(u8"倍镜发光颜色", (float*)zoom_col, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 				}
 				ImGui::EndTabItem();
 			}
 		}
-		
-		/*if (ImGui::BeginTabItem(XorStr("Config")))
+
+		if (ImGui::BeginTabItem(XorStr("Config")))
 		{
-			ImGui::Text(XorStr("Max distance:"));
+			/*ImGui::Text(XorStr("Max distance:"));
 			ImGui::SliderFloat(XorStr("##1"), &max_dist, 100.0f * 40, 800.0f * 40, "%.2f");
 			ImGui::SameLine();
-			ImGui::Text("(%d meters)", (int)(max_dist / 40));
+			ImGui::Text("(%d meters)", (int)(max_dist / 40));*/
 
 			ImGui::Text(XorStr("Smooth aim value:"));
-			ImGui::SliderFloat(XorStr("##2"), &smooth, 12.0f, 150.0f, "%.2f");
+			ImGui::SliderInt(XorStr("##2"), &aSmoothAmount, 3, 10, "%d");
 
-			ImGui::Text(XorStr("Max FOV:"));
-			ImGui::SliderFloat(XorStr("##3"), &max_fov, 5.0f, 250.0f, "%.2f");
+			/*ImGui::Text(XorStr("Max FOV:"));
+			ImGui::SliderFloat(XorStr("##3"), &max_fov, 5.0f, 250.0f, "%.2f");*/
 
 			ImGui::Text(XorStr("Aim at (bone id):"));
 			ImGui::SliderInt(XorStr("##4"), &bone, 0, 175);
@@ -321,7 +329,7 @@ void Overlay::RenderMenu()
 			ImGui::Checkbox(XorStr("Health bar"), &v.healthbar);
 			ImGui::Checkbox(XorStr("Shield bar"), &v.shieldbar);
 			ImGui::EndTabItem();
-		}*/
+		}
 		ImGui::EndTabBar();
 	}
 	ImGui::Text(XorStr("Overlay FPS: %.3f ms/frame (%.1f FPS)"), 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -332,10 +340,21 @@ void Overlay::RenderMenu()
 		ImGui::Checkbox(XorStr(u8"玩家发光"), &player_glow);
 		if (player_glow)
 		{
+			ImGui::Checkbox(XorStr("AIM"), &aim_enable);
+			if (aim_enable)
+			{
+				ImGui::Text(XorStr(u8"自瞄检测窗口大小"));
+				ImGui::SameLine(); HelpMarker(u8"可调整范围为50-300");
+				ImGui::DragInt(XorStr("FOV"), &xFOV, 20, 50, 300, "%d px");
+			}
 			ImGui::ColorEdit4(u8"可见敌人颜色", (float*)playerglow1, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 			ImGui::ColorEdit4(u8"可见倒地颜色", (float*)playerglow2, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 			ImGui::ColorEdit4(u8"不可见敌人颜色", (float*)playerglow3, ImGuiColorEditFlags_AlphaBar | alpha_flags);
 			ImGui::ColorEdit4(u8"不可见倒地颜色", (float*)playerglow4, ImGuiColorEditFlags_AlphaBar | alpha_flags);
+			ImGui::Text(XorStr(u8"发光距离"));
+			ImGui::SameLine(); HelpMarker(u8"可调整范围为0-10000m,ctrl+鼠标单击即可输入值");
+			//ImGui::SliderFloat(XorStr("#dis"), &player_glow_distance, 0.0f, 10000.0f, "%.0fm");
+			ImGui::DragFloat(XorStr("#dis"), &player_glow_distance, 100.0f, 0.0f, 10000.0f, "%.0fm");
 			if (ImGui::Combo(u8"总发光类型", &main_glow2, u8"1\0 2\0 3\0 4\0 5\0 6\0\0"))
 			{
 				switch (main_glow2)
@@ -364,7 +383,7 @@ void Overlay::RenderMenu()
 		ImGui::EndTabBar();
 	}
 	//ImGui::Separator();
-	
+
 	ImGui::End();
 }
 
@@ -612,12 +631,12 @@ void Overlay::DrawLine(ImVec2 a, ImVec2 b, ImColor color, float width)
 	ImGui::GetWindowDrawList()->AddLine(a, b, color, width);
 }
 
-void Overlay::DrawBox(ImColor color, float x, float y, float w, float h)
+void Overlay::DrawBox(ImColor color, float x, float y, float w, float h)//input the position of left bottom corner position
 {
-	DrawLine(ImVec2(x, y), ImVec2(x + w, y), color, 1.0f);
-	DrawLine(ImVec2(x, y), ImVec2(x, y + h), color, 1.0f);
-	DrawLine(ImVec2(x + w, y), ImVec2(x + w, y + h), color, 1.0f);
-	DrawLine(ImVec2(x, y + h), ImVec2(x + w, y + h), color, 1.0f);
+	DrawLine(ImVec2(x, y), ImVec2(x + w, y), color, 1.0f);//bottom
+	DrawLine(ImVec2(x, y), ImVec2(x, y + h), color, 1.0f);//left
+	DrawLine(ImVec2(x + w, y), ImVec2(x + w, y + h), color, 1.0f);//right
+	DrawLine(ImVec2(x, y + h), ImVec2(x + w, y + h), color, 1.0f);//top
 }
 
 void Overlay::Text(ImVec2 pos, ImColor color, const char* text_begin, const char* text_end, float wrap_width, const ImVec4* cpu_fine_clip_rect)
